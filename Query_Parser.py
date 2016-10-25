6df68f9329c15f07a0f4fc2e1281f02a5808cc32
jdnoff <jad217@lehigh.edu>
2016-10-25 17:08:54 -0400

    added comments to query_parser

--- Query_Parser.py
+++ Query_Parser.py
@@ -4,46 +4,47 @@
 import json
 import requests
 
-# CURRENTLY TESTING
-
 # Use Key 1 for testing, Key 2 for Demos/etc.
 API_KEY1 = '2188eefffbb2449f88b73e563deab172'
 API_KEY2 = '62841b36e69d4c3eb4cddfdb7ac56b74'
 
+# Analytic API Constants
 KEYPHRASE_URL = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases"
 ANALYTICS_HEADERS = {'Content-Type': 'application/json',
                      'Ocp-Apim-Subscription-Key': API_KEY1}
 
+#Test Input demonstrating the format of an Analytics request
 TEST_INPUT = {"documents": [
-	{
-		"language": "en",
-		"id": "1",
-		"text": "The increasing volume of data in modern business and science calls for more complex "
-		        "and sophisticated tools. Although advances in data mining technology have made "
-		        "extensive data collection much easier, it's still always evolving and there is a "
-		        "constant need for new techniques and tools that can help us transform this data into "
-		        "useful information and knowledge. Since the previous edition's publication, great "
-		        "advances have been made in the field of data mining. Not only does the third of "
-		        "edition of Data Mining: Concepts and Techniques continue the tradition of equipping "
-		        "you with an understanding and application of the theory and practice of discovering "
-		        "patterns hidden in large data sets, it also focuses on new, important topics in the "
-		        "field: data warehouses and data cube technology, mining stream, mining social "
-		        "networks, and mining spatial, multimedia and other complex data. Each chapter is a "
-		        "stand-alone guide to a critical topic, presenting proven algorithms and sound "
-		        "implementations ready to be used directly or with strategic modification against live "
-		        "data. This is the resource you need if you want to apply today's most powerful data "
-		        "mining techniques to meet real business challenges. * Presents dozens of algorithms "
-		        "and implementation examples, all in pseudo-code and suitable for use in real-world, "
-		        "large-scale data mining projects. * Addresses advanced topics such as mining "
-		        "object-relational databases, spatial databases, multimedia databases, time-series "
-		        "databases, text databases, the World Wide Web, and applications in several fields. "
-		        "*Provides a comprehensive, practical look at the concepts and techniques you need to "
-		        "get the most out of real business data"
-	}
-]
-}
+				{
+					"language": "en",
+					"id": "1",
+					"text": "The increasing volume of data in modern business and science calls for more complex "
+							"and sophisticated tools. Although advances in data mining technology have made "
+							"extensive data collection much easier, it's still always evolving and there is a "
+							"constant need for new techniques and tools that can help us transform this data into "
+							"useful information and knowledge. Since the previous edition's publication, great "
+							"advances have been made in the field of data mining. Not only does the third of "
+							"edition of Data Mining: Concepts and Techniques continue the tradition of equipping "
+							"you with an understanding and application of the theory and practice of discovering "
+							"patterns hidden in large data sets, it also focuses on new, important topics in the "
+							"field: data warehouses and data cube technology, mining stream, mining social "
+							"networks, and mining spatial, multimedia and other complex data. Each chapter is a "
+							"stand-alone guide to a critical topic, presenting proven algorithms and sound "
+							"implementations ready to be used directly or with strategic modification against live "
+							"data. This is the resource you need if you want to apply today's most powerful data "
+							"mining techniques to meet real business challenges. * Presents dozens of algorithms "
+							"and implementation examples, all in pseudo-code and suitable for use in real-world, "
+							"large-scale data mining projects. * Addresses advanced topics such as mining "
+							"object-relational databases, spatial databases, multimedia databases, time-series "
+							"databases, text databases, the World Wide Web, and applications in several fields. "
+							"*Provides a comprehensive, practical look at the concepts and techniques you need to "
+							"get the most out of real business data"
+				}
+			] }
 
 def construct_query(user_input):
+	""" Function to insert user input into correct query format
+		:return: json formatted query """
 	query = { "documents": [
 				{ "language": "en",
 				  "id": "1",
@@ -53,12 +54,15 @@ def construct_query(user_input):
 	return query
 
 def parseQuery(user_input):
+	""" Function to generate a keyword list given user input
+		:return: keyword list """
 	response = requests.post(KEYPHRASE_URL, json=construct_query(user_input) , headers=ANALYTICS_HEADERS)
 	data = response.json()
 	return data
 
 def test_query():
-	""" Test method to generate a keyword list
+	""" Test function to get a stored result of a test query. Use to save requests.
 		:return: keyword list """
 	with open('Analytics_results_test') as data_file:
 		return json.load(data_file)
+
