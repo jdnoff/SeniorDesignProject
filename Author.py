3151ea61748e783cd43fe47922a6433ea3a2ea59
jkh416 <jkh416@lehigh.edu>
2016-10-26 00:55:36 -0400

    fix this

--- Author.py
+++ Author.py
@@ -1,36 +1,35 @@
 # Author object file
 from AS_RequestHandler import test_query
 
+
 class Author:
-    # List of Class Attributes
-    # paperTitles -- list of all papers associated with this author given by the query (given by 'Ti' attribute)
-    # keyWords -- list of all keywords from all papers in paperTitles (given by 'W' attribute)
-    # fieldsOfStudy -- list of all fields of study the papers encompass (given by 'F.FN' attribute)
-    # citationByPaper -- number of citations for each paper in result list (given by 'CC' attribute)
-        # used to sum all total citations for an author
+	# List of Class Attributes
+	# paperTitles -- list of all papers associated with this author given by the query (given by 'Ti' attribute)
+	# keyWords -- list of all keywords from all papers in paperTitles (given by 'W' attribute)
+	# fieldsOfStudy -- list of all fields of study the papers encompass (given by 'F.FN' attribute)
+	# citationByPaper -- number of citations for each paper in result list (given by 'CC' attribute)
+	# used to sum all total citations for an author
 
-    def __init__(self, input):
-        # input should be the json result of a query for an author
-        # which gets the attributes 'Ti', 'W', 'F.FN', 'CC'
-        results = input
-        # results = test_query()
+	def __init__(self, author_name, input):
+		# input should be the json result of a query for an author
+		self.author_name = author_name
+		# which gets the attributes 'Ti', 'W', 'F.FN', 'CC'
+		results = input
+		# results = test_query()
 
-        self.paperTitles = []
-        self.keyWords = []
-        self.fieldsOfStudy = []
-        self.citationByPaper = []
-        for result in results:
-            self.paperTitles.append(result['Ti'].title())
-            self.keyWords.append(result['W'])
-            self.fieldsOfStudy.append(result['F.FN'].title())
-            self.citationByPaper.append(result['CC'])
-        self.totalCitations = self.sumCitations()
+		self.paperTitles = []
+		self.keyWords = []
+		self.fieldsOfStudy = []
+		self.citationByPaper = []
+		for result in results:
+			self.paperTitles = result['Ti']
+			self.keyWords.append(result['W'])
+			self.fieldsOfStudy.append(result['F.FN'])
+			self.citationByPaper.append(result['CC'])
+		self.totalCitations = self.sumCitations()
 
-    def sumCitations(self):
-        total = 0
-        for i in self.citationByPaper:
-            total += self.citationByPaper[i]
-        return total
-
-
-
+	def sumCitations(self):
+		total = 0
+		for i in self.citationByPaper:
+			total += self.citationByPaper[i]
+		return total
--- topic_search.py
+++ topic_search.py
@@ -3,7 +3,7 @@ from AS_RequestHandler import construct_params
 from AS_RequestHandler import evaluate_request
 from Query_Parser import parseQuery
 import academic_constants
-
+from Author import *
 
 # Handler for the topic search use case
 
@@ -15,7 +15,25 @@ def do_topic_search(abstract):
 	query_string = create_query(keyword_list)
 	params = construct_params(query_string, 'latest', '11', '', attributes)
 	data = evaluate_request(params)
-	return compile_author_list(data)
+	return search_list_of_authors(compile_author_list(data))
+
+
+def search_list_of_authors(authorname_list):
+	author_list = []
+	for auth in authorname_list:
+		query = "Composite({}==\'{}\')".format(academic_constants.ATT_AUTHOR_NAME, auth)
+		params = construct_params(query, 'latest', 10, '', {
+			academic_constants.ATT_CITATIONS,
+			academic_constants.ATT_WORDS,
+			academic_constants.ATT_PAPER_TITLE,
+			academic_constants.ATT_FIELD_OF_STUDY
+		})
+		data = evaluate_request(params)
+		print(json.dumps(data))
+		a = Author(auth, data)
+		author_list.append(a)
+	return author_list
+
 
 
 def compile_author_list(data):
