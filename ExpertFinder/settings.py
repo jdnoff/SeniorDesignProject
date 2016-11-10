f2a4fbeae8deccc4ed2001419c8a4d200a7229a2
jkh416 <jkh416@lehigh.edu>
2016-11-10 16:43:12 -0500

    getting things done

--- ExpertFinder/settings.py
+++ ExpertFinder/settings.py
@@ -23,10 +23,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 SECRET_KEY = 'dr#jid42k+0xbm2-d!up79(#vv@@5rf70wquyo8p7oupc(ju15'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    'http://ec2-35-160-88-52.us-west-2.compute.amazonaws.com/'
+    '*'
 ]
 
 
