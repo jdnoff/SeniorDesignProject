4c45bd0856c76d3c8184da00b6a864a78f226ba9
jkh416 <jkh416@lehigh.edu>
2016-12-02 17:27:15 -0500

    topic search caches authors

--- topic_search.py
+++ topic_search.py
@@ -2,14 +2,13 @@ import json
 from AS_RequestHandler import construct_params
 from AS_RequestHandler import evaluate_request
 from Query_Parser import parseQuery
-from Query_Parser import test_query
 import academic_constants
 from Author import *
 from similarity_measure import jaccard_test
 from similarity_measure import keySplit
-import Query_Parser
 from TestResults.test_factory import get_evaluate_test_results
 from nltk.corpus import stopwords
+from django.core.cache import cache
 
 """
 IMPORTANT: run this function and download stopwords corpus from the window:
@@ -33,12 +32,12 @@ def do_topic_search(abstract):
 	}
 	keyword_list = parseQuery(abstract)
 	query_string = create_query(keyword_list)
-	params = construct_params(query_string, '', '10', '', attributes)
+	params = construct_params(query_string, '', '5', '', attributes)
 	real_data = evaluate_request(params)
 	# real_data = get_evaluate_test_results()
 
-	populated_authors = compile_author_list(real_data)
-	search_list_of_authors(populated_authors, keyword_list)
+	authorId_list = compile_author_list(real_data)
+	populated_authors = search_list_of_authors(authorId_list, keyword_list)
 
 	# Compute scores for each author before sending them to be displayed
 	for author in populated_authors:
@@ -58,58 +57,75 @@ def search_list_of_authors(author_list, query_keywords):
 	:return: a list of Author objects
 	"""
 	cachedStopWords = stopwords.words("english")
-	for author in author_list:
-		query = "Composite({}={})".format(academic_constants.ATT_AUTHOR_ID, author.author_id)
-		params = construct_params(query, 'latest', 6, '', {
-			academic_constants.ATT_CITATIONS,
-			academic_constants.ATT_WORDS,
-			academic_constants.ATT_PAPER_TITLE,
-			academic_constants.ATT_FIELD_OF_STUDY,
-			academic_constants.ATT_YEAR,
-			academic_constants.ATT_EXTENDED,
-			academic_constants.ATT_ID,
-			academic_constants.ATT_RERFENCES,
-		})
-		data = evaluate_request(params)
-		# print(json.dumps(data, indent=1))
-		# Authors papers
-		if 'entities' in data:
-			for paper in data['entities']:
-				p = AcademicPaper(paper[ATT_PAPER_TITLE].title())
+	queryKeys = keySplit(query_keywords, cachedStopWords)
+	authors = []
+	for aId in author_list.keys():
+		# Check cache
+		cachedAuthor = cache.get(aId)
+		if not cachedAuthor:
+			# Cache miss
+			author = Author(author_list[aId], aId)
+			query = "Composite({}={})".format(academic_constants.ATT_AUTHOR_ID, aId)
+			params = construct_params(query, 'latest', 2, '', {
+				academic_constants.ATT_CITATIONS,
+				academic_constants.ATT_WORDS,
+				academic_constants.ATT_PAPER_TITLE,
+				academic_constants.ATT_FIELD_OF_STUDY,
+				academic_constants.ATT_YEAR,
+				academic_constants.ATT_EXTENDED,
+				academic_constants.ATT_ID,
+				academic_constants.ATT_RERFENCES,
+			})
+			data = evaluate_request(params)
+			# print(json.dumps(data, indent=1))
+			# Authors papers
+			if 'entities' in data:
+				for paper in data['entities']:
+					p = AcademicPaper(paper[ATT_PAPER_TITLE].title())
 
-				if ATT_CITATIONS in paper:
-					p.addCitations(paper[ATT_CITATIONS])
+					if ATT_CITATIONS in paper:
+						p.addCitations(paper[ATT_CITATIONS])
 
-				if ATT_EXTENDED in paper:
-					desc = json.dumps(paper[ATT_EXTENDED])
-					try:
-						desc = desc.split('\\"')
-						if desc[5] == 'D':
-							p.addDesc(desc[7])
-							abKey = keySplit(parseQuery(desc[7]),cachedStopWords)
-							queryKeys = keySplit(query_keywords, cachedStopWords)
-							p.addKeywords(abKey)
-							score = jaccard_test(queryKeys,abKey)
-							p.addScore(score)
+					if ATT_EXTENDED in paper:
+						desc = json.dumps(paper[ATT_EXTENDED])
+						try:
+							desc = desc.split('\\"')
+							if desc[5] == 'D':
+								p.addDesc(desc[7])
+								abKey = keySplit(parseQuery(desc[7]), cachedStopWords)
+								p.addKeywords(abKey)
+								score = jaccard_test(queryKeys, abKey)
+								p.addScore(score)
 
-						else:
-							p.addDesc("none")
-					except:
-						print("No Abstract")
-						break
+							else:
+								p.addDesc("none")
+						except:
+							print("No Abstract")
+							break
 
-				if ATT_YEAR in paper:
-					p.year = paper[ATT_YEAR]
-				author.addPaper(p)
+					if ATT_YEAR in paper:
+						p.year = paper[ATT_YEAR]
+					author.addPaper(p)
+			authors.append(author)
+			cache.set(aId, author)
+		else:
+			# Cache hit
+			authors.append(cachedAuthor)
+			# Compute score
+			cachedAuthor.score = 0  # Reset score
+			for paper in cachedAuthor.papers:
+				score = jaccard_test(queryKeys, paper.keywords)
+				paper.addScore(score)
+			print("Author= ", cachedAuthor.author_name)
+	return authors
 
 
 # 1
 def compile_author_list(data):
 	"""
-	This is the first step of our query. Translates a json response of papers into a list of Authors
+	This is the first step of our query. Parses the a json response for authors and authorIds. Returning them in a dict
 	:param data: json response from Evaluate request
-	:param query_keywords: keywords parsed from the query
-	:return: a list of Authors
+	:return: a dict of authorIds mapped to author names
 	"""
 	authors = {}
 	if 'entities' in data:
@@ -119,20 +135,11 @@ def compile_author_list(data):
 				auth_id = auth['AuId']
 				auth_name = auth['AuN']
 				if auth_id not in authors.keys():
-					# Create new Author
-					a = Author(author_name=auth_name, author_id=auth_id)
-					authors[auth_id] = a
+					authors[auth_id] = auth_name
 	else:
 		# bad response
 		print("Bad response")
-
-	ret = []
-	i = 0
-	for a in authors.keys():
-		ret.append(authors[a])
-		print("%d: %s" % (i, authors[a].author_name))
-		i += 1
-	return ret
+	return authors
 
 
 def create_query(keyword_list):
